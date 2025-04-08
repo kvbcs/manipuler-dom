daac4fe7bdc7e350fcd5eb580bcae8d6642746cb
Kyllian <kyllianvibancos2@gmail.com>
2025-04-08 14:10:23 +0200

    fin

--- exo6/main.js
+++ exo6/main.js
@@ -1,15 +1,15 @@
-let convert = document.getElementById("convert") ;
+let convert = document.getElementById("convert");
 const secondsInMinute = 60;
 const minutesInHour = 60;
 const hoursInDay = 24;
 const daysInYear = 365;
-let resultSeconds = document.querySelector("#resultSeconds")
-let resultMinutes = document.querySelector("#resultMinutes")
-let resultDays = document.querySelector("#resultDays")
-let resultHours = document.querySelector("#resultHours")
+let resultSeconds = document.querySelector("#resultSeconds");
+let resultMinutes = document.querySelector("#resultMinutes");
+let resultDays = document.querySelector("#resultDays");
+let resultHours = document.querySelector("#resultHours");
 
 convert.addEventListener("click", () => {
- let numberOfYears = document.getElementById("numberOfYears").value ;
-resultDays.innerHTML = numberOfYears * daysInYear + " jours"
-resultHours.innerHTML = numberOfYears * daysInYear * hoursInDay + " heures"
+	let numberOfYears = document.getElementById("numberOfYears").value;
+	resultDays.innerHTML = numberOfYears * daysInYear + " jours";
+	resultHours.innerHTML = numberOfYears * daysInYear * hoursInDay + " heures";
 });
